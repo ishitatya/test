94716e1d1c9c67ccc3c2068bb6636c99f57f98aa
ishitatya <ishitatyagi3314@gmail.com>
2021-08-06 18:44:25 +0530

    box added

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+box10=new Box(50,20,50,50);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    box10.display();
 }
